88f455e081fd59eac2c2c109fe48e2e3f9186186
hhh <1148850672@qq.com>
2019-06-21 15:45:04 +0800

    animation finish

---
+++ src/common/fade/FadeAnimation.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <transition>
+            <slot></slot>  <!-- 外部组件插入进来的插槽 -->
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+      name:'FadeAnimation',  
+    }
+</script>
+
+<style lang='stylus' scoped>
+.v-enter, .v-leave-to
+    opacity: 0
+.v-enter-active .v-leave-active
+    transition: opacity .5s
+</style>
